b8ceb7d1f14cee5e21a75232bc8850639f51c9cb
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-11-23 11:42:20 +0900

    [feature/design-system] Design System Configuration

---
+++ app/src/main/java/org/sopt/stamp/designsystem/style/Color.kt
@@ -0,0 +1,34 @@
+package org.sopt.stamp.designsystem.style
+
+import androidx.compose.ui.graphics.Color
+
+// Purple Scale
+val Purple300 = Color(0xFFC292FF)
+val Purple200 = Color(0xFFEEE1FF)
+val Purple100 = Color(0xFFFAF6FF)
+
+// Pink Scale
+val Pink300 = Color(0xFFFF7EAD)
+val Pink100 = Color(0xFFFFF1F6)
+
+// Mint Scale
+val Mint300 = Color(0xFF53FFC0)
+
+// Red Scale
+val Red300 = Color(0xFFD92B2B)
+val Red200 = Color(0xFFFF8080)
+val Red100 = Color(0xFFFFF7F7)
+
+// Gray Scale
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Gray900 = Color(0xFF151617)
+val Gray800 = Color(0xFF353637)
+val Gray700 = Color(0xFF535455)
+val Gray600 = Color(0xFF666768)
+val Gray500 = Color(0xFF8E8F90)
+val Gray400 = Color(0xFFAEAFB0)
+val Gray300 = Color(0xFFD3D4D5)
+val Gray200 = Color(0xFFE4E5E6)
+val Gray100 = Color(0xFFEFF0F1)
+val Gray50 = Color(0xFFF7F8F9)
