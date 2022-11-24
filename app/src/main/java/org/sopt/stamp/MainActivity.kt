3a0ecb489e8b34bb563e2a94ab2d7ec4cbffe852
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-11-24 20:24:39 +0900

    [feature/util-annotation-add] MultiFormFactorPreviews 추가

--- app/src/main/java/org/sopt/stamp/MainActivity.kt
+++ app/src/main/java/org/sopt/stamp/MainActivity.kt
@@ -2,9 +2,35 @@ package org.sopt.stamp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import org.sopt.stamp.designsystem.style.SoptTheme
+import org.sopt.stamp.util.MultiFormFactorPreviews
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            SoptTheme {
+                Greeting()
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting() {
+    Box {
+        Text("Hello World!")
+    }
+}
+
+@MultiFormFactorPreviews
+@Composable
+fun Preview() {
+    SoptTheme {
+        Greeting()
     }
 }
---
+++ app/src/main/java/org/sopt/stamp/util/MultiFormFactorPreviews.kt
@@ -0,0 +1,17 @@
+package org.sopt.stamp.util
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "phone",
+    device = "spec:shape=Normal,width=360,height=800,unit=dp,dpi=480",
+    showBackground = true,
+    showSystemUi = true
+)
+@Preview(
+    name = "tablet",
+    device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480",
+    showBackground = true,
+    showSystemUi = true
+)
+annotation class MultiFormFactorPreviews()
