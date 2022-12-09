2d66bac63ef66391cbef50b6266f84162c279835
jinsu4755 <parkjinsu4755@gmail.com>
2022-12-09 20:42:20 +0900

    feat: 미션 컴포넌트 작성
    feature/mission-component

---
+++ app/src/main/java/org/sopt/stamp/designsystem/component/mission/CompletedStamp.kt
@@ -0,0 +1,23 @@
+package org.sopt.stamp.designsystem.component.mission
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun CompletedStamp(
+    stamp: Stamp,
+    modifier: Modifier
+) {
+    val completedStamp by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(stamp.lottie))
+    Column {
+        LottieAnimation(
+            composition = completedStamp,
+            modifier = modifier
+        )
+    }
+}
