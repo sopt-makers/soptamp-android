d6ee1973262003fac415e83c34146c3e7a9d516f
jinsu4755 <parkjinsu4755@gmail.com>
2022-12-09 20:41:29 +0900

    feat: 미션 UI Model 작성
    feature/mission-component

---
+++ app/src/main/java/org/sopt/stamp/designsystem/component/mission/model/MissionUiModel.kt
@@ -0,0 +1,11 @@
+package org.sopt.stamp.designsystem.component.mission.model
+
+import org.sopt.stamp.domain.MissionLevel
+
+data class MissionUiModel(
+    val id: Int,
+    val title: String,
+    val level: MissionLevel,
+    val isCompleted: Boolean,
+    val display: Boolean
+)
