74d55d8d1df7792d3779c75b928552096267078d
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-12-09 19:01:06 +0900

    [feature/mission-detail] 기본 Layout 컴포넌트 제작

---
+++ app/src/main/java/org/sopt/stamp/designsystem/component/layout/Layout.kt
@@ -0,0 +1,59 @@
+package org.sopt.stamp.designsystem.component.layout
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Column(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = modifier.padding(horizontal = 20.dp),
+        verticalArrangement,
+        horizontalAlignment,
+        content = content
+    )
+}
+
+@Composable
+fun Row(
+    modifier: Modifier = Modifier,
+    horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
+    verticalAlignment: Alignment.Vertical = Alignment.Top,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        modifier = modifier.padding(horizontal = 20.dp),
+        horizontalArrangement,
+        verticalAlignment,
+        content
+    )
+}
+
+@Composable
+fun Box(
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.TopStart,
+    propagateMinConstraints: Boolean = false,
+    content: @Composable BoxScope.() -> Unit
+) {
+    Box(
+        modifier = modifier.padding(horizontal = 20.dp),
+        contentAlignment,
+        propagateMinConstraints,
+        content
+    )
+}
