af98d1706b1bb86559c0474194f915915c8901fa
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-12-09 18:48:50 +0900

    [feature/mission-detail] RatingBar 제작 완료

---
+++ app/src/main/java/org/sopt/stamp/designsystem/component/ratingbar/RatingBar.kt
@@ -0,0 +1,71 @@
+package org.sopt.stamp.designsystem.component.ratingbar
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import org.sopt.stamp.R
+import org.sopt.stamp.designsystem.style.SoptTheme
+import org.sopt.stamp.util.MultiFormFactorPreviews
+
+private const val DEFAULT_MAX = 3
+
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    @DrawableRes icon: Int,
+    maxStars: Int = DEFAULT_MAX,
+    stars: Int,
+    gapSize: Int,
+    selectedColor: Color = SoptTheme.colors.mint300,
+    unselectedColor: Color = SoptTheme.colors.onSurface30,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(gapSize.dp)
+    ) {
+        repeat(maxStars) {
+            val ordinalIndex = it + 1
+            RatingIcon(
+                icon = icon,
+                tint = if (ordinalIndex > stars) unselectedColor else selectedColor
+            )
+        }
+    }
+}
+
+@Composable
+private fun RatingIcon(
+    @DrawableRes icon: Int,
+    tint: Color
+) {
+    Icon(
+        painter = painterResource(id = icon),
+        contentDescription = "Rating Icon",
+        tint = tint
+    )
+}
+
+@MultiFormFactorPreviews
+@Composable
+fun RatingBarPreview() {
+    SoptTheme {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            RatingBar(
+                icon = R.drawable.ic_star,
+                maxStars = 3,
+                stars = 2,
+                gapSize = 10,
+            )
+        }
+    }
+}
