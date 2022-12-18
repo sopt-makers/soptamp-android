6bd1e7bec476d1189cb100b4c4a2b5668cf9ee36
jinsu4755 <parkjinsu4755@gmail.com>
2022-12-19 07:07:41 +0900

    feat: 랭커 단일 UI 모델 이름 변경
    기존 랭킹이라는 단어 안에는 여러 랭커가 소속 될 수 있어 이름의 뜻을 변경함
    
    feature/ranking

--- app/src/main/java/org/sopt/stamp/feature/ranking/model/RankerUiModel.kt
+++ app/src/main/java/org/sopt/stamp/feature/ranking/model/RankerUiModel.kt
@@ -1,19 +1,18 @@
 package org.sopt.stamp.feature.ranking.model
 
-data class RankingUiModel(
+data class RankerUiModel(
     val rank: Int,
     val user: String,
-    val _description: String? = null,
+    val description: String? = null,
     val score: Int
 ) {
     fun isTopRank() = (rank <= STANDARD_TOP_RANK)
     fun isNotTopRank() = (rank > STANDARD_TOP_RANK)
 
-    val description = _description ?: "설정된 한 마디가 없습니다"
-
     companion object {
         private const val DEFAULT_USER_NAME = "-"
         const val STANDARD_TOP_RANK = 3
-        val DEFAULT_RANK = RankingUiModel(0, DEFAULT_USER_NAME, null, 0)
+        const val DEFAULT_DESCRIPTION = "설정된 한 마디가 없습니다"
+        val DEFAULT_RANK = RankerUiModel(0, DEFAULT_USER_NAME, null, 0)
     }
 }
