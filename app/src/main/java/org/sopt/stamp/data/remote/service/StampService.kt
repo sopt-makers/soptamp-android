b729b0e2b060e8521598024f6fdd56c577859bb5
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-05 16:54:45 +0900

    [feature/net-module] 스탬프 수정 API

--- app/src/main/java/org/sopt/stamp/data/remote/service/StampService.kt
+++ app/src/main/java/org/sopt/stamp/data/remote/service/StampService.kt
@@ -1,7 +1,13 @@
 package org.sopt.stamp.data.remote.service
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import org.sopt.stamp.data.model.response.ResponseModifyStampDto
 import org.sopt.stamp.data.model.response.ResponseStampDto
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface StampService {
@@ -9,4 +15,12 @@ interface StampService {
     suspend fun retrieveStamp(
         @Path("missionId") missionId: Int
     ): ResponseStampDto
+
+    @Multipart
+    @PUT("stamp/{missionId}")
+    suspend fun modifyStamp(
+        @Path("missionId") missionId: Int,
+        @Part stampContent: RequestBody,
+        @Part imageUrl: MultipartBody.Part? = null,
+    ): ResponseModifyStampDto
 }
