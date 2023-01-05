e6f1c5e8d19dbbd21b2896cd43909703411cdb87
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-05 17:46:52 +0900

    fix: lint

--- app/src/main/java/org/sopt/stamp/feature/mission/MissionsState.kt
+++ app/src/main/java/org/sopt/stamp/feature/mission/MissionsState.kt
@@ -5,7 +5,7 @@ import org.sopt.stamp.feature.mission.model.MissionListUiModel
 sealed class MissionsState {
     object Loading : MissionsState()
 
-    data class Success(val missionListUiModel: MissionListUiModel): MissionsState()
+    data class Success(val missionListUiModel: MissionListUiModel) : MissionsState()
 
-    object Failure: MissionsState()
+    object Failure : MissionsState()
 }
