2deae2a775d03381b45ae5c16d1f33ce76c78633
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-06 01:45:33 +0900

    feat: missions response 수정
    feature/mission-list

--- app/src/main/java/org/sopt/stamp/data/remote/model/response/MissionResponse.kt
+++ app/src/main/java/org/sopt/stamp/data/remote/model/response/MissionResponse.kt
@@ -7,6 +7,6 @@ data class MissionResponse(
     val id: Int,
     val title: String,
     val level: Int,
-    val profileImage: List<String>,
+    val profileImage: List<String>?,
     val isCompleted: Boolean
 )
