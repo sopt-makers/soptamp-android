da743d685e88e19f4feadf631b0c37ce75200f36
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-01-06 15:17:52 +0900

    [feature/flipper-config] Flipper Config (#40)

--- app/src/main/java/org/sopt/stamp/App.kt
+++ app/src/main/java/org/sopt/stamp/App.kt
@@ -2,6 +2,12 @@ package org.sopt.stamp
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,5 +15,19 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun initFlipper() {
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            AndroidFlipperClient.getInstance(this).apply {
+                addPlugin(InspectorFlipperPlugin(this@App, DescriptorMapping.withDefaults()))
+                addPlugin(networkFlipperPlugin)
+            }.start()
+        }
+    }
+
+    companion object {
+        val networkFlipperPlugin = NetworkFlipperPlugin()
     }
 }
--- app/src/main/java/org/sopt/stamp/di/ConfigModule.kt
+++ app/src/main/java/org/sopt/stamp/di/ConfigModule.kt
@@ -1,5 +1,6 @@
 package org.sopt.stamp.di
 
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
@@ -7,8 +8,11 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.stamp.App
 import org.sopt.stamp.di.constant.Constant
 import org.sopt.stamp.di.constant.Strings
 import retrofit2.Retrofit
@@ -17,6 +21,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object ConfigModule {
+    @Logging
+    @Provides
+    @Singleton
+    fun providerLoggingInterceptor(): Interceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
     @Provides
     @Singleton
     fun provideSerializer() = Json {
@@ -26,7 +36,11 @@ object ConfigModule {
 
     @Provides
     @Singleton
-    fun provideHttpClient() = OkHttpClient.Builder()
+    fun provideHttpClient(
+        @Logging loggingInterceptor: Interceptor,
+    ) = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .addNetworkInterceptor(FlipperOkhttpInterceptor(App.networkFlipperPlugin))
         .build()
 
     @OptIn(ExperimentalSerializationApi::class)
