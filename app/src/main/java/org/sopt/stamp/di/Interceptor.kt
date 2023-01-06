e9727aa1ccfe8e678e12dd1f0c7029b0cfbdeb4f
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-06 15:01:05 +0900

    [feature/flipper-config] Flipper Config

---
+++ app/src/main/java/org/sopt/stamp/di/Interceptor.kt
@@ -0,0 +1,11 @@
+package org.sopt.stamp.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Logging
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Auth
