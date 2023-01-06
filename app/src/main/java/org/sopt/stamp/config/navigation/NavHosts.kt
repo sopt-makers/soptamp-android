9537a5e3c4d6068fc99e8984a796943ee652ead9
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-01-06 11:59:47 +0900

    [feature/navigation-config] NavHost Setting

--- app/src/main/java/org/sopt/stamp/config/navigation/NavHosts.kt
+++ app/src/main/java/org/sopt/stamp/config/navigation/NavHosts.kt
@@ -1,2 +1,31 @@
 package org.sopt.stamp.config.navigation
 
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+
+/*
+* Splash와 Onboarding 포함한 Screen
+* */
+@RootNavGraph(start = true)
+@NavGraph
+annotation class Initializer(
+    val start: Boolean = false
+)
+
+@RootNavGraph
+@NavGraph
+annotation class Auth(
+    val start: Boolean = false
+)
+
+@RootNavGraph
+@NavGraph
+annotation class Mission(
+    val start: Boolean = false
+)
+
+@RootNavGraph
+@NavGraph
+annotation class Setting(
+    val start: Boolean = false
+)
