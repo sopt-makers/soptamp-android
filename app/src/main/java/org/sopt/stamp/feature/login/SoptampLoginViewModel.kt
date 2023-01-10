9fd5dfea27b0b31a72282afefdd385bde4c52baa
sery270 <sery270@naver.com>
2023-01-10 22:01:18 +0900

    feat: SoptampLoginViewModel login 구현

--- app/src/main/java/org/sopt/stamp/feature/login/SoptampLoginViewModel.kt
+++ app/src/main/java/org/sopt/stamp/feature/login/SoptampLoginViewModel.kt
@@ -6,10 +6,15 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import org.sopt.stamp.domain.repository.UserRepository
+import timber.log.Timber
 import javax.inject.Inject
 
-class SoptampLoginViewModel @Inject constructor() : ViewModel(), LoginHandleAction {
+class SoptampLoginViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel(), LoginHandleAction {
 
     private val _viewState = MutableStateFlow(SoptampLoginViewState.init())
     val viewState = _viewState.asStateFlow()
@@ -20,18 +25,29 @@ class SoptampLoginViewModel @Inject constructor() : ViewModel(), LoginHandleActi
     override fun handleAction(action: LoginAction) {
         when (action) {
             is LoginAction.Login -> login(action.id, action.password)
-            is LoginAction.Success -> TODO()
-            is LoginAction.Fail -> TODO()
+            is LoginAction.LoginFail -> TODO()
         }
     }
 
     private fun login(id: String, password: String) {
         viewModelScope.launch {
-            // TODO login post
-            // TODO viewstate update success || fail
+            userRepository.login(id, password)
+                .onSuccess {
+                    // 200
+                    _singleEvent.trySend(SingleEvent.LoginSuccess)
+
+                    // 400
+                    _viewState.update { prevState ->
+                        prevState.copy(
+                            errorMessage = it.message
+                        )
+                    }
+                }
+                .onFailure {
+                    Timber.tag("test").d(it)
+                }
         }
     }
-
 }
 
 interface LoginHandleAction {
@@ -40,12 +56,12 @@ interface LoginHandleAction {
 
 sealed interface LoginAction {
     data class Login(val id: String, val password: String) : LoginAction
-    data class Success(val id: String, val password: String) : LoginAction
-    data class Fail(val id: String, val password: String) : LoginAction
+    data class LoginFail(val message: String) : LoginAction
 }
 
 sealed interface SingleEvent {
     object Loading : SingleEvent
+    object LoginSuccess : SingleEvent
     object NavigateToJoin : SingleEvent
     object NavigateToFindAccount : SingleEvent
 }
--- app/src/main/java/org/sopt/stamp/feature/login/SoptampLoginViewState.kt
+++ app/src/main/java/org/sopt/stamp/feature/login/SoptampLoginViewState.kt
@@ -2,9 +2,10 @@ package org.sopt.stamp.feature.login
 
 data class SoptampLoginViewState(
     val id: String?,
-    val password: String?
+    val password: String?,
+    val errorMessage: String?
 ) {
     companion object {
-        fun init() = SoptampLoginViewState(null, null)
+        fun init() = SoptampLoginViewState(null, null, null)
     }
 }
