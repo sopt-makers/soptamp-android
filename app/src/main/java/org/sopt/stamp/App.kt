4958083bb4ca4b2dd4b4344e83f8b938bd1df8ac
sery270 <sery270@naver.com>
2023-01-26 01:16:10 +0900

    chore: initFlipper()

--- app/src/main/java/org/sopt/stamp/App.kt
+++ app/src/main/java/org/sopt/stamp/App.kt
@@ -14,6 +14,7 @@ import dagger.hilt.android.HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        initFlipper()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
