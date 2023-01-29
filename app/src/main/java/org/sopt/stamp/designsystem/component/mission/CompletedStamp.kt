31caad3052613418839f36632d9e30c55b4d53bd
jinsu4755 <parkjinsu4755@gmail.com>
2023-01-30 04:12:14 +0900

    fix: CompletedStamp Lottie 를 Image 로 수정
    hotfix/64-mission-stamp-lottie-to-image
    
    ref: #64

--- app/src/main/java/org/sopt/stamp/designsystem/component/mission/CompletedStamp.kt
+++ app/src/main/java/org/sopt/stamp/designsystem/component/mission/CompletedStamp.kt
@@ -1,20 +1,18 @@
 package org.sopt.stamp.designsystem.component.mission
 
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.rememberLottieComposition
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun CompletedStamp(
     stamp: Stamp,
     modifier: Modifier = Modifier
 ) {
-    val completedStamp by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(stamp.lottie))
-    LottieAnimation(
-        composition = completedStamp,
+    Image(
+        painter = painterResource(id = stamp.lottieImage),
+        contentDescription = "Completed Stamp Image",
         modifier = modifier
     )
 }
