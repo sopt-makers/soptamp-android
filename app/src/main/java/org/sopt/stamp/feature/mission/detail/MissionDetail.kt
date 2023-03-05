dd8645b5742714064198a98f0f649e5ccd2d2608
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-03-05 17:27:35 +0900

    :bug: Add horizontal pager in mission detail screen (#93)
    * :bug: Add horizontal pager in mission detail screen
    
    * :rewind: Navhost revert

--- app/src/main/java/org/sopt/stamp/feature/mission/detail/MissionDetail.kt
+++ app/src/main/java/org/sopt/stamp/feature/mission/detail/MissionDetail.kt
@@ -3,6 +3,7 @@ package org.sopt.stamp.feature.mission.detail
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -93,6 +95,7 @@ private fun HeaderView(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun ImageContent(
     imageModel: ImageModel,
@@ -102,61 +105,64 @@ private fun ImageContent(
     val photoPickerLauncher = rememberLauncherForActivityResult(ActivityResultContracts.PickMultipleVisualMedia()) {
         onChangeImage(ImageModel.Local(it))
     }
+    val pageCount = remember(imageModel) { imageModel.size }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(1f)
-            .background(
-                color = SoptTheme.colors.onSurface5,
-                shape = RoundedCornerShape(10.dp)
-            )
-    ) {
+    HorizontalPager(pageCount) { page ->
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .aspectRatio(1f)
                 .background(
                     color = SoptTheme.colors.onSurface5,
                     shape = RoundedCornerShape(10.dp)
                 )
-                .clickable {
-                    photoPickerLauncher.launch(
-                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                    )
-                },
-            contentAlignment = Alignment.Center
         ) {
-            if (isImageEmpty) {
-                Text(
-                    text = "달성 사진을 올려주세요",
-                    style = SoptTheme.typography.sub2,
-                    color = SoptTheme.colors.onSurface50
-                )
-            } else {
-                when (imageModel) {
-                    is ImageModel.Local -> {
-                        AsyncImage(
-                            model = imageModel.uri[0],
-                            contentDescription = "",
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clip(RoundedCornerShape(10.dp)),
-                            contentScale = ContentScale.Crop
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = SoptTheme.colors.onSurface5,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .clickable {
+                        photoPickerLauncher.launch(
+                            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
                         )
-                    }
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                if (isImageEmpty) {
+                    Text(
+                        text = "달성 사진을 올려주세요",
+                        style = SoptTheme.typography.sub2,
+                        color = SoptTheme.colors.onSurface50
+                    )
+                } else {
+                    when (imageModel) {
+                        is ImageModel.Local -> {
+                            AsyncImage(
+                                model = imageModel.uri[page],
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clip(RoundedCornerShape(10.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
 
-                    is ImageModel.Remote -> {
-                        AsyncImage(
-                            model = imageModel.url[0],
-                            contentDescription = "",
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clip(RoundedCornerShape(10.dp)),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
+                        is ImageModel.Remote -> {
+                            AsyncImage(
+                                model = imageModel.url[page],
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .clip(RoundedCornerShape(10.dp)),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
 
-                    else -> throw IllegalStateException("예외처리 했으므로 여긴 안 통과함")
+                        else -> throw IllegalStateException("예외처리 했으므로 여긴 안 통과함")
+                    }
                 }
             }
         }
